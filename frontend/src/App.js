b69eba9b89921343e565e7230d6d420ed4d801a6
KavinduAnj <wkanjana1590@gmail.com>
2025-12-13 18:50:56 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-     Hello !
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
