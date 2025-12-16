6485b33eafaef85562ab1276fc4098e739e11729
KavinduAnj <wkanjana1590@gmail.com>
2025-12-16 21:37:33 +0530

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Books />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="/update" element={<Update />} />
     </Routes>
   );
 }
