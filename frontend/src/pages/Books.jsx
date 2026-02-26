52f12fcf4805002933f811d8ce42969e85458b41
KavinduAnj <wkanjana1590@gmail.com>
2026-02-26 22:23:34 +0530

    delete books

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -16,6 +16,15 @@ const Books = () => {
   }
   fetchAllBooks();
   }, []);
+
+  const handleDelete = async (id) => {
+    try{
+      await axios.delete("http://localhost:8800/books/"+id);
+      window.location.reload();
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
     <div>
       <h1>Books</h1>
@@ -26,7 +35,7 @@ const Books = () => {
             <h2>{book.author}</h2>
             <p>{book.description}</p>
             <span>{book.price}</span>
-            <button className='delete'>Delete</button>
+            <button className='delete'onClick={()=>handleDelete(book.id)}>Delete</button>
             <button className='update'>Update</button>
           </div>
         ))}
